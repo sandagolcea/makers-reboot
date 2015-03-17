a8c3a0fed3fae5d2c132e53b497cb46c7d160afe
Sanda Golcea <sanda.golcea@gmail.com>
2015-03-17 16:34:37 +0000

    wk1-bb release and dock bikes with tests

--- w1-boris-bikes/spec/docking_station_spec.rb
+++ w1-boris-bikes/spec/docking_station_spec.rb
@@ -2,4 +2,10 @@ require 'docking_station'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
+  it 'can dock a bike' do
+    expect(subject.dock :bike).to eq :bike
+  end
+  it 'raises error when no bikes are available' do
+    expect { subject.release_bike } .to raise_error 'No bikes available'
+  end
 end
