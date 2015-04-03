a1ced1d99e1629864a22afad06e13daea06fb9ed
Sanda Golcea <sanda.golcea@gmail.com>
2015-04-03 23:42:58 +0100

    Battleships added board for placing ship

--- w3-battleships-web/lib/battleships.rb
+++ w3-battleships-web/lib/battleships.rb
@@ -1,15 +1,17 @@
 require 'sinatra/base'
-require './lib/player'
+require './lib/game'
 
 class BattleShips < Sinatra::Base
   set :views, proc { File.join(root, '..', 'views') }
+  set :public_folder, proc { File.join(root, '..', 'public') }
+
   get '/' do
     erb :index
   end
 
   get '/game' do
-    session[:player1] = Player.new(params[:player1])
-    @player1 = session[:player1]
+    session[:game] = Game.new(params[:player1], params[:player2])
+    @game = session[:game]
     erb :game
   end
 
